3b7ee4c85a8ec8e0d830eb4611491fd365de8b76
Yael Hadad <yael0556734@gmail.com>
2024-02-22 23:36:41 +0200

    Add project files.

---
+++ exc1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace exc1
+{
+    class SwapList<T> : List<T>
+    {
+        public void swap(int i,int j)
+        {
+            T k = this[i];
+            this[i] = this[j];
+            this[j] = k;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            PrintTheObj(x);
+            SwapList<int> arr = new SwapList<int>() { 5, 6, 7 };
+            arr.swap(0, 1);
+            PrintTheObj(arr);
+            PrintTheObj(arr);
+        }
+        static void PrintTheObj<T>(T obj)
+        {
+            Console.WriteLine(obj);
+            Console.Read();
+        }
+    }
+}
